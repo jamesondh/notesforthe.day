689e4bbd2a05eed3d301edd728ff67c623c60778
Jameson Hodge <contact@jamesonhodge.com>
2024-04-17 10:46:55 -0400

    style checked items in checkboxes list

--- src/components/checkboxes.tsx
+++ src/components/checkboxes.tsx
@@ -1,9 +1,16 @@
+import classNames from "classnames";
+
 interface CheckboxProps {
   list: string[];
   checkedState: { [key: string]: boolean };
   onChange: (item: string) => void;
   onRemove: (item: string) => void;
 }
+
+const itemBackground = (checked: boolean) =>
+  checked ? "bg-gray-900" : "bg-gray-800";
+const itemStrikethrough = (checked: boolean) =>
+  checked ? "line-through" : "none";
 
 export default function Checkboxes({
   list,
@@ -14,7 +21,12 @@ export default function Checkboxes({
   return (
     <div>
       {list.map((item) => (
-        <div className="rounded my-1 py-2 bg-gray-800">
+        <div
+          className={classNames(
+            "rounded my-1 py-2",
+            itemBackground(checkedState[item]),
+          )}
+        >
           <label className="flex justify-between" key={item}>
             <div className="ml-4">
               <input
@@ -23,7 +35,13 @@ export default function Checkboxes({
                 onChange={() => onChange(item)}
                 className="mr-2 mb-2"
               />
-              {item}
+              <span
+                style={{
+                  textDecoration: itemStrikethrough(checkedState[item]),
+                }}
+              >
+                {item}
+              </span>
             </div>
             <button className="mr-4 btn" onClick={() => onRemove(item)}>
               Remove
